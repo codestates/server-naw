3b70ba2329c52bceb9e9339d5be12a3334bb6708
Rockheung <rockheung@gmail.com>
2019-03-29 21:33:48 +0900

    Success get request

--- routes/mna.js
+++ routes/mna.js
@@ -3,14 +3,13 @@ const fs = require("fs");
 var express = require("express");
 var router = express.Router();
 
-const fakePdfData = fs.readFileSync(
-  __dirname + "/../fakeData/fakeMnaList.json",
-  "utf-8"
+const fakeMnaList = JSON.parse(
+  fs.readFileSync(__dirname + "/../fakeData/fakeMnaList.json", "utf-8")
 );
 
 /* GET mna listing. */
 router.get("/:id", function(req, res, next) {
-  res.send(JSON.parse(fakePdfData));
+  res.send(JSON.stringify(fakeMnaList[req.params.id]));
 });
 
 module.exports = router;
