0e9e5762f4c78791b6e208a30ab0cffd2ca1467d
Rockheung <rockheung@gmail.com>
2019-03-29 20:42:16 +0900

    Endpoint for mnas

---
+++ routes/mna.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+var express = require("express");
+var router = express.Router();
+
+const fakePdfData = fs.readFileSync(
+  __dirname + "/../fakeData/fakeMnaList.json",
+  "utf-8"
+);
+
+/* GET mna listing. */
+router.get("/:id", function(req, res, next) {
+  res.send(JSON.parse(fakePdfData));
+});
+
+module.exports = router;
