e9ad59b6c804c23fc57f590fdbb63b2ca84c7ad2
Rockheung <rockheung@gmail.com>
2019-03-30 13:59:22 +0900

    Update fakeData

--- utils/attendanceRate.js
+++ utils/attendanceRate.js
@@ -1,18 +1,19 @@
 const fs = require("fs");
 
-const fakePdfData = JSON.parse(
-  fs.readFileSync("./fakeData/fakePdfData.json", "utf-8")
-);
-const fakeMnaList = JSON.parse(
-  fs.readFileSync("./fakeData/fakeMnaList.json", "utf-8")
-);
-const fakeDangList = JSON.parse(
-  fs.readFileSync("./fakeData/fakeDangList.json", "utf-8")
-);
+// const fakePdfData = JSON.parse(
+//   fs.readFileSync("./fakeData/fakePdfData.json", "utf-8")
+// );
+// const fakeMnaList = JSON.parse(
+//   fs.readFileSync("./fakeData/fakeMnaList.json", "utf-8")
+// );
+// const fakeDangList = JSON.parse(
+//   fs.readFileSync("./fakeData/fakeDangList.json", "utf-8")
+// );
 
-module.exports.attendanceRate = id => {
+// mnaName: string ,
+module.exports.attendanceRate = (mna_id, fakeMnaList, fakePdfData) => {
   return {
-    mainRate: 0.7,
-    subRate: 0.9
+    rateForCongress: 0.7,
+    rateForStandingCongress: 0.9
   };
 };
