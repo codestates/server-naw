40c6741975c0557963f0e3aeda0db6a843919594
Rockheung <rockheung@gmail.com>
2019-04-04 01:06:40 +0900

    recover MNA's method from dev commit

--- models/MNA.js
+++ models/MNA.js
@@ -66,5 +66,30 @@ module.exports = (sequelize, DataTypes) => {
     );
     MNA.belongsTo(models.LOCAL, { foreignKey: "LOCAL_ID" });
   };
+
+  MNA.getList = () => {
+    return sequelize
+      .query(
+        "select * from MNA as M INNER JOIN LOCAL AS L INNER JOIN PARTY AS P ON M.LOCAL_ID = L.LOCAL_ID AND M.PARTY_ID = P.PARTY_ID ORDER BY M.MNA_ID;",
+        {
+          model: MNA,
+          mapToModel: true
+        }
+      )
+      .then(MNA => MNA);
+  };
+
+  MNA.findMna = id => {
+    return sequelize
+      .query(
+        `select * from MNA as M INNER JOIN LOCAL AS L INNER JOIN PARTY AS P ON M.MNA_ID = ${id} AND M.LOCAL_ID = L.LOCAL_ID AND M.PARTY_ID = P.PARTY_ID ORDER BY M.MNA_ID;`,
+        {
+          model: MNA,
+          mapToModel: true
+        }
+      )
+      .then(MNA => MNA);
+  };
+
   return MNA;
 };
