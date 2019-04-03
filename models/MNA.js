e74f732af9e275ef446fbba9a42c76e39908789c
devych <ych900809@gmail.com>
2019-04-03 19:33:39 +0900

    add DB feature findMna

--- models/MNA.js
+++ models/MNA.js
@@ -1,69 +1,101 @@
 /* jshint indent: 1 */
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('MNA', {
-		MNA_ID: {
-			type: DataTypes.INTEGER(11),
-			allowNull: false,
-			primaryKey: true
-		},
-		MNA_NAME: {
-			type: DataTypes.STRING(45),
-			allowNull: true
-		},
-		MNA_HANJA: {
-			type: DataTypes.STRING(45),
-			allowNull: true
-		},
-		MNA_PHOTO: {
-			type: DataTypes.TEXT,
-			allowNull: true
-		},
-		PARTY_ID: {
-			type: DataTypes.INTEGER(11),
-			allowNull: true,
-			references: {
-				model: 'PARTY',
-				key: 'party_id'
-			}
-		},
-		LOCAL_ID: {
-			type: DataTypes.INTEGER(11),
-			allowNull: true,
-			references: {
-				model: 'LOCAL',
-				key: 'local_id'
-			}
-		},
-		MNA_HISTORY: {
-			type: DataTypes.TEXT,
-			allowNull: true
-		},
-		STDCOMT_TEXT: {
-			type: DataTypes.TEXT,
-			allowNull: true
-		},
-		MNA_PHONE: {
-			type: DataTypes.STRING(30),
-			allowNull: true
-		},
-		MNA_EMAIL: {
-			type: DataTypes.STRING(255),
-			allowNull: true
-		},
-		MNA_ADDRESS: {
-			type: DataTypes.STRING(255),
-			allowNull: true
-		},
-		MNA_HOMEPAGE: {
-			type: DataTypes.TEXT,
-			allowNull: true
-		},
-		MNA_URL: {
-			type: DataTypes.TEXT,
-			allowNull: true
-		}
-	}, {
-		tableName: 'MNA'
-	});
+  const MNA = sequelize.define(
+    "MNA",
+    {
+      MNA_ID: {
+        type: DataTypes.INTEGER(11),
+        allowNull: false,
+        primaryKey: true
+      },
+      MNA_NAME: {
+        type: DataTypes.STRING(45),
+        allowNull: true
+      },
+      MNA_HANJA: {
+        type: DataTypes.STRING(45),
+        allowNull: true
+      },
+      MNA_PHOTO: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      PARTY_ID: {
+        type: DataTypes.INTEGER(11),
+        allowNull: true,
+        references: {
+          model: "PARTY",
+          key: "party_id"
+        }
+      },
+      LOCAL_ID: {
+        type: DataTypes.INTEGER(11),
+        allowNull: true,
+        references: {
+          model: "LOCAL",
+          key: "local_id"
+        }
+      },
+      MNA_HISTORY: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      STDCOMT_TEXT: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      MNA_PHONE: {
+        type: DataTypes.STRING(30),
+        allowNull: true
+      },
+      MNA_EMAIL: {
+        type: DataTypes.STRING(255),
+        allowNull: true
+      },
+      MNA_ADDRESS: {
+        type: DataTypes.STRING(255),
+        allowNull: true
+      },
+      MNA_HOMEPAGE: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      MNA_URL: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      }
+    },
+    {
+      tableName: "MNA"
+    }
+  );
+
+  MNA.associate = function(models) {};
+
+  MNA.getList = () => {
+    return sequelize
+      .query(
+        "select * from MNA as M INNER JOIN LOCAL AS L INNER JOIN PARTY AS P ON M.LOCAL_ID = L.LOCAL_ID AND M.PARTY_ID = P.PARTY_ID ORDER BY M.MNA_ID;",
+        {
+          model: MNA,
+          mapToModel: true
+        }
+      )
+      .then(MNA => MNA);
+  };
+
+  MNA.findMna = id => {
+    return sequelize
+      .query(
+        `select * from MNA as M INNER JOIN LOCAL AS L INNER JOIN PARTY AS P ON M.MNA_ID = ${id} AND M.LOCAL_ID = L.LOCAL_ID AND M.PARTY_ID = P.PARTY_ID ORDER BY M.MNA_ID;`,
+        {
+          model: MNA,
+          mapToModel: true
+        }
+      )
+      .then(MNA => MNA);
+  };
+
+  return MNA;
 };
--- routes/mna.js
+++ routes/mna.js
@@ -2,33 +2,56 @@ const fs = require("fs");
 
 var express = require("express");
 var router = express.Router();
+var MNA = require("../models").MNA;
 
 // const { attendanceRate } = require("../utils/attendanceRate.js");
 
-const fakeMnaList = JSON.parse(
-  fs.readFileSync(__dirname + "/../fakeData/fakeMnaList.json", "utf-8")
-);
+// const fakeMnaList = JSON.parse(
+//   fs.readFileSync(__dirname + "/../fakeData/fakeMnaList.json", "utf-8")
+// );
 
-const fakePdfData = JSON.parse(
-  fs.readFileSync("./fakeData/fakePdfData.json", "utf-8")
-).meetings;
+// const fakePdfData = JSON.parse(
+//   fs.readFileSync("./fakeData/fakePdfData.json", "utf-8")
+// ).meetings;
+
+router.get("/all", (req, res, next) => {
+  MNA.getList()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.error(error);
+      res.status(500).setHeader(error);
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  MNA.findMna(req.params.id)
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.error(error);
+      res.status(500).setHeader(error);
+    });
+});
 
 /* GET mna listing. */
-router.get("/:id", function(req, res, next) {
-  var getMna = fakeMnaList.filter(mna => {
-    return mna.id === +req.params.id;
-  })[0];
+// router.get("/:id", function(req, res, next) {
+//   var getMna = fakeMnaList.filter(mna => {
+//     return mna.id === +req.params.id;
+//   })[0];
 
-  Object.assign(getMna, attendanceRate(getMna, fakePdfData));
-  if (getMna) {
-    res
-      .status(200)
-      .type("json")
-      .send(JSON.stringify(getMna));
-    return;
-  }
+//   Object.assign(getMna, attendanceRate(getMna, fakePdfData));
+//   if (getMna) {
+//     res
+//       .status(200)
+//       .type("json")
+//       .send(JSON.stringify(getMna));
+//     return;
+//   }
 
-  res.status(403).end();
-});
+//   res.status(403).end();
+// });
 
 module.exports = router;
