62835a0db82c380bc104e8ce7357a07521397996
Rockheung <rockheung@gmail.com>
2019-04-05 17:46:57 +0900

    Update router: mna

--- routes/mna.js
+++ routes/mna.js
@@ -29,8 +29,38 @@ router.get("", (req, res, next) => {
 router.get("/:id", (req, res, next) => {
   Mna.findMna(req.params.id)
     .then(result => {
-      console.log(result);
-      res.status(200).json(result);
+      let mnaData = result[0].dataValues;
+      let mnaDataObj = {
+        id: mnaData.MNA_ID,
+        name: mnaData.MNA_NAME,
+        chinesename: mnaData.MNA_HANJA,
+        photo: mnaData.MNA_PHOTO,
+        party_id: mnaData.PARTY_ID,
+        local: "서울 동작구을",
+        stdcomt: mnaData.STDCOMT_TEXT,
+        phone: mnaData.MNA_PHONE,
+        address: "의원회관 450호",
+        email: mnaData.MNA_EMAIL,
+        mna_url: "https://twitter.com/nakw",
+        history: mnaData.MNA_HISTORY,
+        stdcomtAttendanceRate: 0.88,
+        stdcomtAttendanceRatePosition: 20,
+        stdcomtBillSubmitCount: 20,
+        stdcomtBillSubmitCountPosition: 20,
+        billsPerCommittee: [
+          { name: "법제사법위원회", value: 8 },
+          { name: "교육위원회", value: 5 },
+          { name: "환경노동위원회", value: 5 },
+          { name: "보건복지위원회", value: 4 },
+          { name: "행정안전위원회", value: 8 }
+        ],
+        billsPerStatus: [
+          { name: "계류", value: 34 },
+          { name: "대안반영폐기", value: 3 },
+          { name: "원안가결", value: 3 }
+        ]
+      };
+      res.status(200).json(mnaDataObj);
     })
     .catch(err => {
       console.log(err);
