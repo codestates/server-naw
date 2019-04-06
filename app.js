7186e48b62eac22c30710cf9a5644698335a398c
PaDuckk <kang10019@naver.com>
2019-04-06 13:42:11 +0900

    JWT 기반 인증 구현

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes");
+var v1Router = require('./routes/v1.js');
 var usersRouter = require("./routes/users.js");
 var mnaRouter = require("./routes/mna.js");
 
@@ -12,13 +13,15 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
-
 app.use("/", indexRouter);
+app.use("/v1", v1Router);
 app.use("/users", usersRouter);
 app.use("/mna", mnaRouter);
 
